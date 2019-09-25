9ca5e88547b16be3231f5aa116d9081503e0af98
rodrigodisconsi@gmail.com <RodrigoDisconsi@gmail.com>
2019-09-25 16:34:08 -0300

    25/9

---
+++ Ejercicios visual estudio/Ejercicio 37/CentralTelefonica/Ejercicio N°40/Central Telefónica.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CentralitaHerencia;
+
+namespace Ejercicio_N_40
+{
+    public partial class FormMenu : Form
+    {
+        Centralita centralita;
+        public FormMenu()
+        {
+            InitializeComponent();
+            this.centralita = new Centralita("Centralita Form");
+        }
+    }
+}
