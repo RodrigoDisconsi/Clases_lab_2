9fd6f40cd42bafe369ddcc6dabe34a742091a2e2
rodrigodisconsi@gmail.com <RodrigoDisconsi@gmail.com>
2019-10-02 13:12:27 -0300

    2/10

---
+++ Ejercicios visual estudio/Primer Parcial/ComiqueriaApp/ComiqueriaLogic/Comic.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComiqueriaLogic
+{
+    public class Comic : Producto
+    {
+        private string autor;
+        private TipoComic tipoComic;
+
+        public Comic(string descripcion, int stock, double precio, string autor, TipoComic tipoComic) : base(descripcion, stock, precio)
+        {
+            this.autor = autor;
+            this.tipoComic = tipoComic;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder rtn = new StringBuilder();
+            rtn.AppendLine(base.ToString());
+            rtn.AppendLine($"Autor: {this.autor}");
+            rtn.AppendLine($"Tipo de comic: {this.tipoComic}");
+            return rtn.ToString();
+        }
+
+        public enum TipoComic
+        {
+            Occidental,
+            Oriental
+        }
+    }
+}
---
+++ Ejercicios visual estudio/Primer Parcial/ComiqueriaApp/ComiqueriaLogic/Venta.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComiqueriaLogic
+{
+    public sealed class Venta
+    {
+        private DateTime fecha;
+        private static int porcentajeIva;
+        private double precioFinal;
+        private Producto producto;
+
+        static Venta()
+        {
+            porcentajeIva = 21;
+        }
+
+        public static double CalcularPrecioFinal(double precioUnidad, int cantidad)
+        {
+            double precioSinIva = precioUnidad * cantidad;
+            double precioIva = precioSinIva * 21 / 100;
+            return precioSinIva + precioIva;
+        }
+
+        private void Vender(int cantidad)
+        {
+
+        }
+
+    }
+}
