d0f9032ca35bedec358a4795344cbb528f6c46ce
alumno <RodrigoDisconsi@gmail.com>
2019-11-13 21:54:08 -0300

    Practica parcial

---
+++ Ejercicios visual estudio/Practica Segunda Parcial/20180628 - SP Lab II/20180628-SP - Alumno/Entidades/DAO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class DAO
+    {
+        string stringConecction;
+
+        public DAO()
+        {
+            this.stringConecction = "Data Source=.\\SQLEXPRESS;" +
+                "Initial Catalog=votacion-sp-2018;Integrated Security=True";
+        }
+    }
+}
---
+++ Ejercicios visual estudio/Practica Segunda Parcial/20180628 - SP Lab II/20180628-SP - Alumno/Entidades/IArchivos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public interface IArchivos <T>
+    {
+        T Leer(string path);
+        bool Guardar(string path, T objeto);
+    }
+}
